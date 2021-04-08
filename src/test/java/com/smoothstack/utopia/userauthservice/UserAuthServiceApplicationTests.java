4544c1a0ebdda90da561a0b4f9705968a68b36ad
Rob Maes <robert.maes@smoothstack.com>
2021-04-08 12:48:11 -0500

    fix test

--- src/test/java/com/smoothstack/utopia/userauthservice/UserAuthServiceApplicationTests.java
+++ src/test/java/com/smoothstack/utopia/userauthservice/UserAuthServiceApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(
+  locations = "classpath:application-integrationtest.properties"
+)
 class UserAuthServiceApplicationTests {
 
   @Test
