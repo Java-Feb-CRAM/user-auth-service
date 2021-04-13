cf0d5d3c83afc3405d9054632690da8ba67f395b
Rob Maes <robert.maes@smoothstack.com>
2021-04-13 13:52:31 -0500

    remove code smells

--- src/main/java/com/smoothstack/utopia/userauthservice/authentication/service/UserService.java
+++ src/main/java/com/smoothstack/utopia/userauthservice/authentication/service/UserService.java
@@ -1,17 +1,11 @@
 /**
- * 
+ *
  */
 package com.smoothstack.utopia.userauthservice.authentication.service;
 
-import java.time.LocalDateTime;
-import javax.transaction.Transactional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.smoothstack.utopia.shared.model.PasswordResetToken;
 import com.smoothstack.utopia.shared.model.User;
+import com.smoothstack.utopia.shared.model.UserRole;
 import com.smoothstack.utopia.shared.model.VerificationToken;
 import com.smoothstack.utopia.userauthservice.authentication.dto.CredentialsDto;
 import com.smoothstack.utopia.userauthservice.authentication.dto.PasswordResetDto;
@@ -28,6 +22,12 @@ import com.smoothstack.utopia.userauthservice.dao.UserRepository;
 import com.smoothstack.utopia.userauthservice.dao.UserRoleRepository;
 import com.smoothstack.utopia.userauthservice.dao.VerificationTokenRepository;
 import com.smoothstack.utopia.userauthservice.security.JwtUtil;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Craig Saunders
@@ -36,121 +36,160 @@ import com.smoothstack.utopia.userauthservice.security.JwtUtil;
 @Service
 @Transactional
 public class UserService {
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
-	private UserRoleRepository userRoleRepository;
-	@Autowired
-	private VerificationTokenRepository verificationTokenRepository;
-	@Autowired
-	private PasswordResetTokenRepository passwordResetTokenRepository;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	@Autowired
-	private JwtUtil jwtUtil;
 
-	public boolean validatePasswordResetToken(String token) throws InvalidTokenException {
+  @Autowired
+  private UserRepository userRepository;
 
-		PasswordResetToken resetToken = passwordResetTokenRepository.findByToken(token)
-				.orElseThrow(InvalidTokenException::new);
-		if ((resetToken.getExpiryDate().isBefore(LocalDateTime.now()))) {
-			passwordResetTokenRepository.delete(resetToken);
-			return false;
-		}
-		return true;
-	}
+  @Autowired
+  private UserRoleRepository userRoleRepository;
 
-	private void setUserActive(String token) {
-		User user = verificationTokenRepository.findByToken(token).orElseThrow(InvalidTokenException::new).getUser();
-		user.setActive(true);
-		userRepository.save(user);
-	}
+  @Autowired
+  private VerificationTokenRepository verificationTokenRepository;
 
-	public boolean validateUserAccountVerificationToken(String token) throws InvalidTokenException {
-		VerificationToken verificationToken = verificationTokenRepository.findByToken(token)
-				.orElseThrow(InvalidTokenException::new);
-		if ((verificationToken.getExpiryDate().isBefore(LocalDateTime.now()))) {
-			verificationTokenRepository.delete(verificationToken);
-			throw new VerificationTokenExpiredException();
-		}
+  @Autowired
+  private PasswordResetTokenRepository passwordResetTokenRepository;
 
-		if (verificationToken.getUser().isActive()) {
-			verificationTokenRepository.delete(verificationToken);
-			return false;
-		}
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-		setUserActive(token);
-		verificationTokenRepository.delete(verificationToken);
-		return true;
-	}
+  @Autowired
+  private JwtUtil jwtUtil;
 
-	public User registerNewUserAccount(UserDto userDto)
-			throws UserAlreadyExistException, UnmatchedPasswordException {
-		if (!userRepository.findByEmail(userDto.getEmail()).isEmpty()) {
-			throw new UserAlreadyExistException();
-		}
-		if (!userRepository.findByUsernameIgnoreCase(userDto.getUsername()).isEmpty()) {
-			throw new UserAlreadyExistException();
-		}
-		if (userDto.getPassword().equals(userDto.getMatchingPassword())) {
-			User user = new User();
-			user.setGivenName(userDto.getGivenName());
-			user.setFamilyName(userDto.getFamilyName());
-			user.setUsername(userDto.getUsername());
-			user.setPassword(passwordEncoder.encode(userDto.getPassword()));
-			user.setEmail(userDto.getEmail());
-			user.setPhone(userDto.getPhone());
-			user.setUserRole(userRoleRepository.findByName("ROLE_USER").get());
-			return userRepository.save(user);
-		} else {
-			throw new UnmatchedPasswordException();
-		}
-	}
+  public boolean validatePasswordResetToken(String token)
+    throws InvalidTokenException {
+    PasswordResetToken resetToken = passwordResetTokenRepository
+      .findByToken(token)
+      .orElseThrow(InvalidTokenException::new);
+    if ((resetToken.getExpiryDate().isBefore(LocalDateTime.now()))) {
+      passwordResetTokenRepository.delete(resetToken);
+      return false;
+    }
+    return true;
+  }
 
-	public void createVerificationTokenForUser(String token, String username) throws InvalidCredentialsException {
-		User user = userRepository.findByUsername(username).orElseThrow(InvalidCredentialsException::new);
+  private void setUserActive(String token) {
+    User user = verificationTokenRepository
+      .findByToken(token)
+      .orElseThrow(InvalidTokenException::new)
+      .getUser();
+    user.setActive(true);
+    userRepository.save(user);
+  }
 
-		verificationTokenRepository.findByUser(user).ifPresentOrElse(t -> verificationTokenRepository.delete(t),
-				new Runnable() {
-					@Override
-					public void run() {
-						verificationTokenRepository.save(new VerificationToken(token, user));
-					}
-				});
-	}
+  public boolean validateUserAccountVerificationToken(String token)
+    throws InvalidTokenException {
+    VerificationToken verificationToken = verificationTokenRepository
+      .findByToken(token)
+      .orElseThrow(InvalidTokenException::new);
+    if ((verificationToken.getExpiryDate().isBefore(LocalDateTime.now()))) {
+      verificationTokenRepository.delete(verificationToken);
+      throw new VerificationTokenExpiredException();
+    }
 
-	public void createPasswordResetTokenForUser(String token, String username) throws InvalidCredentialsException {
-		User user = userRepository.findByUsername(username).orElseThrow(InvalidCredentialsException::new);
+    if (verificationToken.getUser().isActive()) {
+      verificationTokenRepository.delete(verificationToken);
+      return false;
+    }
 
-		passwordResetTokenRepository.findAllByUser(user).stream().forEach(t -> passwordResetTokenRepository.delete(t));
-		passwordResetTokenRepository.save(new PasswordResetToken(token, user));
-	}
+    setUserActive(token);
+    verificationTokenRepository.delete(verificationToken);
+    return true;
+  }
 
-	public void changeUserPassword(PasswordResetDto passwordDto)
-			throws UnmatchedPasswordException, InvalidCurrentPasswordException, PasswordResetTokenExpiredException {		
-		PasswordResetToken passwordResetToken = passwordResetTokenRepository.findByToken(passwordDto.getToken())
-				.orElseThrow(InvalidTokenException::new);
-		if (passwordResetToken.getExpiryDate().isAfter(LocalDateTime.now())) {
-			User user = passwordResetToken.getUser();
-			if (passwordEncoder.matches(passwordDto.getCurrentPassword(), user.getPassword())) {
-				if (passwordDto.getNewPassword().equals(passwordDto.getConfirmNewPassword())) {
-					user.setPassword(passwordEncoder.encode(passwordDto.getNewPassword()));
-				} else {
-					throw new UnmatchedPasswordException();
-				}
-			} else {
-				throw new InvalidCurrentPasswordException();
-			}
+  public User registerNewUserAccount(UserDto userDto)
+    throws UserAlreadyExistException, UnmatchedPasswordException {
+    if (!userRepository.findByEmail(userDto.getEmail()).isEmpty()) {
+      throw new UserAlreadyExistException();
+    }
+    if (
+      !userRepository.findByUsernameIgnoreCase(userDto.getUsername()).isEmpty()
+    ) {
+      throw new UserAlreadyExistException();
+    }
+    if (userDto.getPassword().equals(userDto.getMatchingPassword())) {
+      User user = new User();
+      user.setGivenName(userDto.getGivenName());
+      user.setFamilyName(userDto.getFamilyName());
+      user.setUsername(userDto.getUsername());
+      user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+      user.setEmail(userDto.getEmail());
+      user.setPhone(userDto.getPhone());
+      Optional<UserRole> userRole = userRoleRepository.findByName("ROLE_USER");
+      userRole.ifPresent(user::setUserRole);
+      return userRepository.save(user);
+    } else {
+      throw new UnmatchedPasswordException();
+    }
+  }
 
-			userRepository.save(user);
-			passwordResetTokenRepository.delete(passwordResetToken);
-		} else {
-			passwordResetTokenRepository.delete(passwordResetToken);
-			throw new PasswordResetTokenExpiredException();
-		}
-	}
+  public void createVerificationTokenForUser(String token, String username)
+    throws InvalidCredentialsException {
+    User user = userRepository
+      .findByUsername(username)
+      .orElseThrow(InvalidCredentialsException::new);
 
-	public String authenticateUser(CredentialsDto credentialsDto) {
-		return jwtUtil.getAuthenticatedJwt(credentialsDto.getUsername(), credentialsDto.getPassword());
-	}
+    verificationTokenRepository
+      .findByUser(user)
+      .ifPresentOrElse(
+        t -> verificationTokenRepository.delete(t),
+        () ->
+          verificationTokenRepository.save(new VerificationToken(token, user))
+      );
+  }
+
+  public void createPasswordResetTokenForUser(String token, String username)
+    throws InvalidCredentialsException {
+    User user = userRepository
+      .findByUsername(username)
+      .orElseThrow(InvalidCredentialsException::new);
+
+    passwordResetTokenRepository
+      .findAllByUser(user)
+      .stream()
+      .forEach(t -> passwordResetTokenRepository.delete(t));
+    passwordResetTokenRepository.save(new PasswordResetToken(token, user));
+  }
+
+  public void changeUserPassword(PasswordResetDto passwordDto)
+    throws UnmatchedPasswordException, InvalidCurrentPasswordException, PasswordResetTokenExpiredException {
+    PasswordResetToken passwordResetToken = passwordResetTokenRepository
+      .findByToken(passwordDto.getToken())
+      .orElseThrow(InvalidTokenException::new);
+    if (passwordResetToken.getExpiryDate().isAfter(LocalDateTime.now())) {
+      User user = passwordResetToken.getUser();
+      if (
+        passwordEncoder.matches(
+          passwordDto.getCurrentPassword(),
+          user.getPassword()
+        )
+      ) {
+        if (
+          passwordDto
+            .getNewPassword()
+            .equals(passwordDto.getConfirmNewPassword())
+        ) {
+          user.setPassword(
+            passwordEncoder.encode(passwordDto.getNewPassword())
+          );
+        } else {
+          throw new UnmatchedPasswordException();
+        }
+      } else {
+        throw new InvalidCurrentPasswordException();
+      }
+
+      userRepository.save(user);
+      passwordResetTokenRepository.delete(passwordResetToken);
+    } else {
+      passwordResetTokenRepository.delete(passwordResetToken);
+      throw new PasswordResetTokenExpiredException();
+    }
+  }
+
+  public String authenticateUser(CredentialsDto credentialsDto) {
+    return jwtUtil.getAuthenticatedJwt(
+      credentialsDto.getUsername(),
+      credentialsDto.getPassword()
+    );
+  }
 }
